b33f06f30b0df9fe46253f72aafaf092bf2ceba9
Luis Herrera <pipeh.ing@gmail.com>
2024-08-22 14:22:56 -0500

    feat: add Foo repository, @primary annotation

---
+++ src/main/java/com/lab/pipefehecar/springboot/di/repositories/ProductRepositoryFoo.java
@@ -0,0 +1,25 @@
+package com.lab.pipefehecar.springboot.di.repositories;
+
+import com.lab.pipefehecar.springboot.di.models.Product;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collections;
+import java.util.List;
+
+@Primary
+@Repository
+public class ProductRepositoryFoo implements ProductRepository {
+
+
+    @Override
+    public List<Product> getAll() {
+        return Collections.singletonList(new Product(1L, "Asus Monitor 27\" 2K ", 1332000L));
+    }
+
+
+    @Override
+    public Product getById(Long id) {
+        return new Product(1L, "Asus Monitor 27\" 2K ", 1332000L);
+    }
+}
